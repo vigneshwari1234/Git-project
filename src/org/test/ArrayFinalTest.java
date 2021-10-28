54d49bf81d87bcb49812629c06ca398ceaac9bc7
vigneshwari1234 <vigneshwari123kumar@gmail.com>
2021-10-28 14:47:47 +0530

    mani

---
+++ src/org/test/ArrayFinalTest.java
@@ -0,0 +1,38 @@
+package org.test;
+
+public class ArrayFinalTest {
+
+	public static void main(String[] args) {
+		
+		int a[]=new int[5];
+	    a[0]=10;
+	    a[1]=20;
+	    a[2]=10;
+	    a[3]=40;
+	    a[4]=50;
+	    
+	    		
+	    
+	    //int len = a.length;
+	    //System.out.println(len);
+	    
+	   //
+	    //System.out.println(a[2]);
+	    
+	    //iteration
+	    
+	   // for (int i = 0; i < a.length; i++) {
+			
+	    	//System.out.println(a[i]);
+	//	}
+	    
+	  for(int x:a)  {
+		  System.out.println(x);
+	  }
+	    
+
+	    		
+		
+		
+	}
+}
